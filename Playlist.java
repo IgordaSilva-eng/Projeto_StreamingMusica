f66ee1b5d8f936fc3239ee38e0d2ddbd75a00a8d
IgordaSilva-eng <dasilvaalvescorreaigor@gmail.com>
2026-03-27 08:10:43 -0300

    Preparando entrega CP2

---
+++ Playlist.java
@@ -0,0 +1,42 @@
+import java.util.ArrayList;
+
+public class Playlist {
+
+    String nome;
+    ArrayList<Musica> musicas = new ArrayList<>();
+
+    public Playlist(String nome) {
+        this.nome = nome;
+    }
+
+    public void adicionarMusica(Musica musica) {
+        this.musicas.add(musica);
+    }
+
+    public void removerMusica(int indice) {
+        if (indice >= 0 && indice < this.musicas.size()) {
+            this.musicas.remove(indice);
+        } else {
+            System.out.println("Erro: Índice de música inválido.");
+        }
+    }
+
+    public void listarMusicas() {
+        System.out.println("\n--- Músicas da Playlist: " + this.nome + " ---");
+        for (Musica m : this.musicas) {
+            m.exibir();
+        }
+    }
+
+    public int getDuracaoTotal() {
+        int total = 0;
+        for (Musica m : this.musicas) {
+            total += m.duracaoSegundos;
+        }
+        return total;
+    }
+
+    public int getQuantidadeMusicas() {
+        return this.musicas.size();
+    }
+}
