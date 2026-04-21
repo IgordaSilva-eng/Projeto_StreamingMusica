46ae936f14bf59b4636be7a468ee6f44511e8973
IgordaSilva-eng <dasilvaalvescorreaigor@gmail.com>
2026-04-21 03:18:18 -0300

    adição da subclasse UsuarioFree

---
+++ src/UsuarioFree.java
@@ -0,0 +1,17 @@
+public class UsuarioFree extends Usuario {
+    private int contadorReproducoes = 0;
+
+    public UsuarioFree(String nome, String email) {
+        super(nome, email); // Chama o construtor da superclasse
+    }
+
+    @Override
+    public void reproduzirMusica(Musica musica) {
+        contadorReproducoes++;
+        // Lógica de anúncio a cada 3 músicas [cite: 61, 84]
+        if (contadorReproducoes % 3 == 0) {
+            System.out.println("\nANÚNCIO: Assine Premium e ouça sem interrupções!");
+        }
+        super.reproduzirMusica(musica); // Reutiliza o método do pai
+    }
+}
